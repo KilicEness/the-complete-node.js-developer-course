3294470f8f628edd350644c400d4335d5cbe3e4f
enes <eneskiliicc06@gmail.com>
2023-03-02 17:28:42 +0300

    handling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,13 @@
 const request = require('request')
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.207233&lon=33.191018&appid=9e89611818623bc7428b1e27629313e3&units=metric'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.207233&lon=33.191018&appid={YourAPI}&units=metric'
 
-request({ url: url, json: true}, (error, response) => {
-    //ı need help for acces to weather description
-    console.log(response.body.weather.description + ' It is currently ' + response.body.main.temp + ' feels like ' + response.body.main.feels_like)
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to weather app')
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log(response.body.weather[0].description + ' It is currently ' + response.body.main.temp + ' feels like ' + response.body.main.feels_like)
+    }
 })
