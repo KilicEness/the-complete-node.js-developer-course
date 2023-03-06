9f1bf36489451292b3b334a10e438a2e6cf2148f
enes <eneskiliicc06@gmail.com>
2023-03-06 16:06:23 +0300

    404 page and styling application

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,21 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Enes Kilic',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Enes Kilic',
+        errorMessage: 'Page not found'
+    })
+})
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
