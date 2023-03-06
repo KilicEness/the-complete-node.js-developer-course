c925e8665343845bc963e32fe7872c1e61cf0053
enes <eneskiliicc06@gmail.com>
2023-03-06 12:06:50 +0300

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,31 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../views'));
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Enes'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Enes Kilic'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'how can i help you',
+        name: 'Enes Kilic'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
