34c1a37901ac640403196fa9df8a375dbfea6eb5
enes <eneskiliicc06@gmail.com>
2023-03-15 17:41:51 +0300

    set up mongodb and insert documents

---
+++ task-manager/mongodb.js
@@ -0,0 +1,40 @@
+// CRUD CREATE read update delete
+const { MongoClient } = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+const client = new MongoClient(url);
+ 
+async function getData() {
+  try {
+    const db = client.db(databaseName);
+ 
+    // db.collection("users").insertOne({
+    //   name: "Enes",
+    //   age: 22,
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'clean the house',
+            completed: false
+        }, {
+            description: 'renew inspection',
+            completed: false
+        }, {
+            description: 'pot plants',
+            completed: false
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result.ops)
+    })
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+ 
+getData();
