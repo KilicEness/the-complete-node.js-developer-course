2e2797ddd20d4cdbd61a5f4456dfde242a78def1
enes <eneskiliicc06@gmail.com>
2023-03-16 10:01:28 +0300

    explore Object id

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,14 @@
 // CRUD CREATE read update delete
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const url = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 const client = new MongoClient(url);
  
+const id = new ObjectId()
+console.log(id)
+console.log(id.id)
+console.log(id.toHexString().length)
+
 async function getData() {
   try {
     const db = client.db(databaseName);
@@ -13,24 +18,24 @@ async function getData() {
     //   age: 22,
     // });
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'clean the house',
-            completed: false
-        }, {
-            description: 'renew inspection',
-            completed: false
-        }, {
-            description: 'pot plants',
-            completed: false
-        }
-    ], (error, result) => {
-        if(error) {
-            return console.log('Unable to insert tasks')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'clean the house',
+    //         completed: false
+    //     }, {
+    //         description: 'renew inspection',
+    //         completed: false
+    //     }, {
+    //         description: 'pot plants',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert tasks')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
 
   } catch (err) {
     console.log(err);
