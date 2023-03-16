08667201ceda75c52c84bb9c536db89f29983d73
enes <eneskiliicc06@gmail.com>
2023-03-16 10:32:14 +0300

    querying documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,43 +3,27 @@ const { MongoClient, ObjectId } = require("mongodb");
 const url = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 const client = new MongoClient(url);
- 
-const id = new ObjectId()
-console.log(id)
-console.log(id.id)
-console.log(id.toHexString().length)
 
 async function getData() {
   try {
     const db = client.db(databaseName);
- 
-    // db.collection("users").insertOne({
-    //   name: "Enes",
-    //   age: 22,
-    // });
+    db.collection('tasks').findOne({ _id: new ObjectId("6411d83a0ce6135f8e0c721e") }).then((result, error) => {
+      if (error) {
+        return console.log('Unable to fetch')
+      }
+      console.log(`${result.description} is that`)
+    })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'clean the house',
-    //         completed: false
-    //     }, {
-    //         description: 'renew inspection',
-    //         completed: false
-    //     }, {
-    //         description: 'pot plants',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert tasks')
-    //     }
-
-    //     console.log(result.ops)
-    // })
+    db.collection('tasks').find({ completed: false }).toArray().then((result, error) => {
+      if (error) {
+        return console.log('Unable to fetch')
+      }
+      console.log(result)
+    })
 
   } catch (err) {
     console.log(err);
   }
 }
- 
+
 getData();
