72d1893511ec785f0e9e75612ceb4e4091637de7
enes <eneskiliicc06@gmail.com>
2023-03-17 10:39:00 +0300

    data validation and sanitization I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 main().catch(err => console.log(err))
 
@@ -7,21 +8,65 @@ async function main() {
 
     const User  = mongoose.model('User', {
         name: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         age: {
-            type: Number
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number')
+                }
+            }
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Please provide an email')
+                }
+            }
         }
     })
 
     const me = new User({
-        name: 'Enes',
-        age: 22
+        name: 'Michael Scott  ',
+        email: '  mIchaelScoTT@gmail.com'
     })
 
     me.save().then(() => {
         console.log(me)
     }).catch((error) => {
         console.log('Error!', error)
+    }) 
+
+    /* const Task = mongoose.model('Task', {
+        description: {
+            type: String
+        }, 
+        completed: {
+            type: Boolean
+        }
     })
+
+    const task = new Task({
+        description: 'Learn Mongoose',
+        completed: false
+    })
+
+    task.save().then(() => {
+        console.log(task)
+    }).catch((error) => {
+        console.log(error)
+    }) */
+
+
+
+
+
 }
