1dfae15075526a5f24edc8977c729a0fc6974202
enes <eneskiliicc06@gmail.com>
2023-03-17 15:51:46 +0300

    structuring a rest api, resource creation endpoints

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,61 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 main().catch(err => console.log(err))
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
-
-    const User  = mongoose.model('User', {
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        age: {
-            type: Number,
-            default: 0,
-            validate(value) {
-                if (value < 0) {
-                    throw new Error('Age must be a positive number')
-                }
-            }
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error('Please provide an email')
-                }
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 7,
-            validate(value) {
-                if (value.toLowerCase().includes('password')) {
-                    throw new Error('Password cannot contain password')
-                }
-            }
-        }
-    })
-
-   /*  const me = new User({
-        name: 'Michael Scott  ',
-        email: '  mIchaelScoTT@gmail.com',
-        password: '         password        '
-    })
-
-    me.save().then(() => {
-        console.log(me)
-    }).catch((error) => {
-        console.log('Error!', error)
-    })  */
 
     const Task = mongoose.model('Task', {
         description: {
@@ -69,15 +17,6 @@ async function main() {
         }
     })
 
-    const task = new Task({
-        description: '  eat lunch'
-    })
-
-    task.save().then(() => {
-        console.log(task)
-    }).catch((error) => {
-        console.log(error)
-    })
 
 
 
---
+++ task-manager/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const User = require('./models/user')
+require('./db/mongoose')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
