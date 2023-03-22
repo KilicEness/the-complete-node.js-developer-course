4ac92c7a4c46030502c1e332f4672a181475107a
enes <eneskiliicc06@gmail.com>
2023-03-22 11:00:27 +0300

    generating authentication tokens

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //Define our user model
 const userSchema = new mongoose.Schema({
@@ -40,8 +41,24 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password cannot contain password')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
