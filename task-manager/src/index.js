a23d39ca00bf30a671bbe8a6bddfee950b9b22ed
enes <eneskiliicc06@gmail.com>
2023-03-24 12:13:58 +0300

    file upload operations to the task app

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,10 +7,6 @@ require('./db/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
-/* app.use((req, res, next) => {
-    res.status(503).send('Site is currently down')
-}) */
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -82,7 +84,51 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+//Upload profile avatar
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
 
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+//Delete profile avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+//Find users avatar by id
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
 
 
 module.exports = router
