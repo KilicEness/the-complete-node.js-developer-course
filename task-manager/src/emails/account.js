e3ed6aae06a43e1df191bfe3339784405c1e9e6a
enes <eneskiliicc06@gmail.com>
2023-03-27 11:13:23 +0300

    sending welcome and cancelation emails

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,17 +1,25 @@
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'test@example.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'user@example.com',
+    subject: 'Thanks for joining in',
+    text: `Welcome to the app, ${name}, Let me know how you get along with me`
+  })
 }
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'user@example.com',
+    subject: 'sorry to see you go',
+    text: `Goodbye ${name}, I hope to see you back again`
   })
-  .catch((error) => {
-    console.error(error)
-  })
+}
+
+  module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+  }
