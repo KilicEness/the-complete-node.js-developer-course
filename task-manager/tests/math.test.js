59be6bb8870c866253be87e40d818e9532544819
enes <eneskiliicc06@gmail.com>
2023-03-27 15:29:13 +0300

    writing your own tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,31 @@
-test('Hello World', () => {
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
 })
 
-test('This should be fail', () => {
-    throw new Error('Failure')
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
 })
+
+test('Should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
+})
+
+/* Why Test?
+-Saves time
+-Creates reliable software
+-Gives flexibility to developers
+-Refactoring
+-Collaborating
+-Profiling
+-Peace of mind
+ */
