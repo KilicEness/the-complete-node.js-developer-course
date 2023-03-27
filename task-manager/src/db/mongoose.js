179958eed81b2f69cfe4be709437590664f9ca0c
enes <eneskiliicc06@gmail.com>
2023-03-27 11:46:22 +0300

    environment variables

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 main().catch(err => console.log(err))
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
+    await mongoose.connect(process.env.MONGODB_URL)
 
     
 
