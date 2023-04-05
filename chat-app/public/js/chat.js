299fcdd49f02e2bf218e2f4396793146fd22d30d
enes <eneskiliicc06@gmail.com>
2023-04-05 08:27:06 +0300

    rendering user list

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -34,6 +35,15 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('H:mm')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+//event listener sidebar
+socket.on('roomData',( { room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 //client side send message event
